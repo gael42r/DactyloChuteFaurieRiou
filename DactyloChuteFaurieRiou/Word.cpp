91a6bd5e2bf05d62c5fb634f9122542ccde30413
Gaël Riou PC <gael.louis.riou@etu.univ-st-etienne.fr>
2021-05-16 17:57:12 +0200

    qgigfdzq

--- DactyloChuteFaurieRiou/Word.cpp
+++ DactyloChuteFaurieRiou/Word.cpp
@@ -8,7 +8,7 @@ Word::Word(string content)
 {
 	content_ = content;
 	state_ = 0;
-	currentChar_ = 2;
+	currentChar_ = 3;
 
 	text_.setCharacterSize(25);
 	text_.setPosition(rand() % 1920, 10);
@@ -17,8 +17,8 @@ Word::Word(string content)
 void Word::draw(sf::RenderWindow& renderWindow, sf::Font& font)
 {
 	string fisrtPart = "%3" + content_.substr(0, currentChar_);
-	string secondPart = "%4" + content_.substr(currentChar_, 1);
-	string thirdPart = "%5" + content_.substr(currentChar_ + 1, content_.length() - currentChar_);
+	string secondPart = "%5" + content_.substr(currentChar_, 1);
+	string thirdPart = "%2" + content_.substr(currentChar_ + 1, content_.length() - currentChar_);
 	text_.setFont(font);
 	text_.setString(fisrtPart + secondPart + thirdPart);
 	renderWindow.draw(text_);

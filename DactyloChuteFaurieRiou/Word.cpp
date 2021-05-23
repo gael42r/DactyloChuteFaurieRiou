01c1bacf03b94701ba294fed1fdf5865618589cb
Gaël Riou PC <gael.louis.riou@etu.univ-st-etienne.fr>
2021-05-23 22:51:20 +0200

    mots en size 30

--- DactyloChuteFaurieRiou/Word.cpp
+++ DactyloChuteFaurieRiou/Word.cpp
@@ -4,7 +4,7 @@ Word::Word(string content)
 {
 	content_ = content;
 
-	pos_.x = rand() % 1520 + 200;
+	pos_.x = rand() % 1400 + 260;
 	pos_.y = 0;
 
 	currentCaracter_ = 0;
@@ -22,6 +22,7 @@ void Word::drawCurrent(sf::RenderWindow& renderWindow)
 	sf::Font font;
 	font.loadFromFile(".\\Resources\\arial.ttf");
 	text_.setFont(font);
+	text_.setCharacterSize(30);
 	string alreadyEntered = content_.substr(0, currentCaracter_);
 	string currentCharacter = content_.substr(currentCaracter_, 1);
 	string stillToBeEntered = content_.substr(currentCaracter_ + 1, content_.size());
@@ -38,7 +39,7 @@ void Word::drawCurrent(sf::RenderWindow& renderWindow)
 			text_ << sf::Color::White << alreadyEntered << sf::Color::Cyan << currentCharacter << sf::Color::Yellow << stillToBeEntered;
 		}
 	}
-	text_.setCharacterSize(25);
+	text_.setOrigin(text_.getLocalBounds().left + text_.getLocalBounds().width / 2.0f, text_.getLocalBounds().top + text_.getLocalBounds().height / 2.0f);
 	text_.setPosition(pos_);	
 	pos_.y = pos_.y + 3;
 	renderWindow.draw(text_);
@@ -50,10 +51,10 @@ void Word::draw(sf::RenderWindow& renderWindow)
 	sf::Font font;
 	font.loadFromFile(".\\Resources\\arial.ttf");
 	text_.setFont(font);
+	text_.setCharacterSize(30);
 	text_.clear();
 	text_ << sf::Color::Yellow << content_;
-	
-	text_.setCharacterSize(25);
+	text_.setOrigin(text_.getLocalBounds().left + text_.getLocalBounds().width / 2.0f, text_.getLocalBounds().top + text_.getLocalBounds().height / 2.0f);
 	text_.setPosition(pos_);
 	pos_.y = pos_.y + 3;
 	renderWindow.draw(text_);
